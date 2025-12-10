f3c0fbffc0d08631aacb6ac0e0ba8b985cc24ad4
Jonathan-Smith9410 <jsmith300599@googlemail.com>
2025-12-10 11:59:00 +0000

    Exercise completed

--- lib/vowel_remover.py
+++ lib/vowel_remover.py
@@ -1,4 +1,20 @@
 # File: lib/vowel_remover.py
+# Original code:
+# class VowelRemover:
+#     def __init__(self, text):
+#         self.text = text
+#         self.vowels = ["a", "e", "i", "o", "u"]
+
+#     def remove_vowels(self):
+#         i = 0
+#         while i < len(self.text):
+#             if self.text[i].lower() in self.vowels:
+#                 self.text = self.text[:i] + self.text[i+1:]
+#             i += 1
+#         return self.text
+
+
+# Debugged code:
 
 class VowelRemover:
     def __init__(self, text):
@@ -10,5 +26,7 @@ class VowelRemover:
         while i < len(self.text):
             if self.text[i].lower() in self.vowels:
                 self.text = self.text[:i] + self.text[i+1:]
-            i += 1
+            else:
+                i += 1
         return self.text
+    
